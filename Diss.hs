084509ca32979a1e1ebfdd396c4d338d66e43472
gabrielhurst11 <gjhurst1@sheffield.ac.uk>
2024-01-12 15:11:31 +0000

    CNF Conversion

--- Diss.hs
+++ Diss.hs
@@ -232,6 +232,9 @@ proofExample = do
     printNDTree conclusion
 
 
+type Clause = [Prop]
+type ClauseSet = [Clause]
+
 elimImp :: Prop -> Prop
 elimImp (Imply p q) = Or (Not (elimImp p)) (elimImp q)
 elimImp (And p q) = And (elimImp p) (elimImp q)
@@ -254,10 +257,33 @@ distribute (And p q)        = And (distribute p) (distribute q)
 distribute (Or p q)         = Or (distribute p) (distribute q)
 distribute p                = p 
 
+-- Function to check if a clause contains contradictory propositions
+containsContradiction :: Clause -> Bool
+containsContradiction clause = any (isNegationPresent clause) clause
 
+-- Check if the negation of a proposition is present in the clause
+isNegationPresent :: Clause -> Prop -> Bool
+isNegationPresent clause prop = case prop of
+    Not p -> p `elem` clause
+    _     -> Not prop `elem` clause
+
+-- Function to filter out clauses with contradictions
+filterContradictions :: ClauseSet -> ClauseSet
+filterContradictions = filter (not . containsContradiction)
+
+convertToClauses :: Prop -> ClauseSet
+convertToClauses (And p q) = convertToClauses p ++ convertToClauses q
+convertToClauses p         = [convertToClause p]
+
+convertToClause :: Prop -> Clause
+convertToClause (Or p q) = convertToClause p ++ convertToClause q
+convertToClause p        = [p]
 
 convertCNF :: Prop -> Prop
 convertCNF p = distribute(pushNegation (elimImp(p)))
 
 pTest :: Prop
 pTest = Imply (Or (Var 'P') (Var 'Q')) (Or (Var 'Q') (Var 'R'))
+
+convertAndFilterCNF :: Prop -> ClauseSet
+convertAndFilterCNF p = filterContradictions(convertToClauses(convertCNF p))
