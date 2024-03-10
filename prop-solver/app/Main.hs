b2fae893841b28ad18e703cbdc0649522955da68
gabrielhurst11 <gjhurst1@sheffield.ac.uk>
2024-03-10 15:37:06 +0000

    Set up Websockets

--- prop-solver/app/Main.hs
+++ prop-solver/app/Main.hs
@@ -1,6 +1,12 @@
 module Main where
     
 
+import Network.WebSockets
+import Control.Monad (forever)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Text.Encoding (encodeUtf8)
+
 import Examples
 import Propositional
 import Cnf
@@ -8,9 +14,19 @@ import Functions
 
 main :: IO ()
 main = do
-    putStrLn "Welcome to the Propositional Logic Solver!"
-    putStrLn "Here are some example propositions:"
-    print p1
-    print p2
+    putStrLn "WebSocket server started"
+    runServer "127.0.0.1" 8080 application
 
-
+application :: ServerApp
+application pending = do
+    conn <- acceptRequest pending
+    putStrLn "Client connected"
+    sendTextData conn (encodeUtf8 $ T.pack "Welcome to the Propositional Logic Solver!")
+    sendTextData conn (encodeUtf8 $ T.pack "Here are some example propositions:")
+    sendTextData conn (encodeUtf8 $ T.pack (show p1))
+    sendTextData conn (encodeUtf8 $ T.pack (show p2))
+    forever $ do
+        msg <- receiveData conn
+        putStrLn $ "Received message: " ++ show (msg :: T.Text)
+        -- Process the message received from the client and send a response
+        sendTextData conn (encodeUtf8 $ T.pack ("Received: " <> T.unpack msg))
