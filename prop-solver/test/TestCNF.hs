cf1f6244c61ad52bf952b2d8aa2e6a070e8acfb0
gabrielhurst11 <gjhurst1@sheffield.ac.uk>
2024-04-12 16:08:57 +0100

    More Tests

--- prop-solver/test/TestCNF.hs
+++ prop-solver/test/TestCNF.hs
@@ -10,9 +10,27 @@ pImpliesQ = Imply (Var 'P') (Var 'Q')
 notPOrQ :: Prop
 notPOrQ = Or (Not (Var 'P')) (Var 'Q')
 
+notPAndQ :: Prop
+notPAndQ = (Not (And (Var 'P') (Var 'Q')))
+
+notPOrNotQ :: Prop
+notPOrNotQ = Or (Not (Var 'P')) (Not (Var 'Q'))
+
+pAndQ_Or_POrR :: Prop
+pAndQ_Or_POrR = Or (And (Var 'P') (Var 'Q')) (Or (Var 'Q') (Var 'R')) 
+
+pOrQOrR_And_QOrQOrR :: Prop
+pOrQOrR_And_QOrQOrR = And (Or (Var 'P') (Or (Var 'Q') (Var 'R'))) (Or (Var 'Q') (Or (Var 'Q') (Var 'R')))
+
 elimImp1 :: Test
 elimImp1 = TestCase(assertEqual "Eliminate Implication Test 1" (elimImp pImpliesQ) notPOrQ)
 
+pushNegation1 :: Test
+pushNegation1 = TestCase(assertEqual "Push Negation Test 1" (pushNegation notPAndQ) notPOrNotQ)
+
+distribute1 :: Test
+distribute1 = TestCase(assertEqual "Distribution Test 1" (distribute pAndQ_Or_POrR) pOrQOrR_And_QOrQOrR)
+
 
 tests :: Test
-tests = TestList [elimImp1]
+tests = TestList [elimImp1, pushNegation1, distribute1]
